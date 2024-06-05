ec8cf48fd172481326c689db4613819281607cf2
marcelo.lopes@jimmydin.com.br <marcelo.lopes@jimmydin.com.br>
2024-06-05 16:33:23 -0300

    DEV-01 Herois sendo apresentados quando selecionados

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 export type Hero = {
   img: string,
   nome: string,
-  texto: string
+  texto: string,
+  id: number
 }
 
 @Component({
@@ -13,27 +14,49 @@ export type Hero = {
 })
 export class HeroComponent implements OnInit {
 
-  harley: Hero = {
-    img: "",
-    nome: "Harley",
-    texto: "Texto"
-  }
+  heroiSelecionado: Hero;
+
    herois: Hero[] = [
     {
-      img: "src\assets\aamon.jpg",
+      img: "./assets/aamon.jpg",
       nome: "Aamon",
-      texto: "Texto"
+      texto: "Texto",
+      id: 1
     },
     {
-      img: "",
+      img: "./assets/Lesley.jpg",
       nome: "Lesley",
-      texto: "Texto"
-    }
+      texto: "Lesley cresceu na casa de Vance, onde seu pai presidiu a segurança da família nobre. Durante um ataque de uma família rival, o pai de Lesley foi morto. Quando os assaltantes se aproximaram da casa de Vance, Lesley pegou o longo rifle preto de seu pai e cumpriu a missão de seu falecido pai.",
+      id: 2
+    },
+    {
+      img: "./assets/harley.jpg",
+      nome: "Harley",
+      texto: "Texto",
+      id: 3
+    },
+    {
+      img: "./assets/gusion.jpg",
+      nome: "Gusion",
+      texto: "Texto",
+      id: 4
+    },
+    {
+      img: "./assets/fredrinn.jpg",
+      nome: "Fredrinn",
+      texto: "Texto",
+      id: 5
+    },
    ]
 
   constructor() { }
 
   ngOnInit(): void {
+    this.heroiSelecionado = this.herois.find(item => item.id == 2  );
   }
 
+  selecionarHeroi(id: number){
+    console.log(this.heroiSelecionado);
+    this.heroiSelecionado = this.herois.find(item => item.id == id  );
+  }
 }

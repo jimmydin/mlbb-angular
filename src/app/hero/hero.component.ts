859665207618be45154bed80732da138d125fe23
marcelo.lopes@jimmydin.com.br <marcelo.lopes@jimmydin.com.br>
2024-06-10 18:53:24 -0300

    DEV-06 Incluindo Parentes

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -4,7 +4,8 @@ export type Hero = {
   img: string,
   nome: string,
   texto: string,
-  id: number
+  id: number,
+  parentes?: number[]
 }
 
 @Component({
@@ -16,18 +17,22 @@ export class HeroComponent implements OnInit {
 
   heroiSelecionado: Hero;
 
+  parentesHeroiSelecionado: Hero[];
+
    herois: Hero[] = [
     {
       img: "./assets/aamon.jpg",
       nome: "Aamon",
       texto: "Texto",
-      id: 1
+      id: 1,
+      parentes: [5]
     },
     {
       img: "./assets/Lesley.jpg",
       nome: "Lesley",
       texto: "Lesley cresceu na casa de Vance, onde seu pai presidiu a segurança da família nobre. Durante um ataque de uma família rival, o pai de Lesley foi morto. Quando os assaltantes se aproximaram da casa de Vance, Lesley pegou o longo rifle preto de seu pai e cumpriu a missão de seu falecido pai.",
-      id: 2
+      id: 2,
+      parentes: [1, 3, 4, 5]
     },
     {
       img: "./assets/harley.jpg",
@@ -52,11 +57,15 @@ export class HeroComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.heroiSelecionado = this.herois.find(item => item.id == 2  );
+    this.selecionarHeroi(2);
   }
 
   selecionarHeroi(id: number){
-    console.log(this.heroiSelecionado);
     this.heroiSelecionado = this.herois.find(item => item.id == id  );
+    if(!this.heroiSelecionado.parentes){
+      this.parentesHeroiSelecionado = [];
+    } else{
+      this.parentesHeroiSelecionado = this.herois.filter(heroi => this.heroiSelecionado.parentes.includes(heroi.id));
+    }
   }
 }
